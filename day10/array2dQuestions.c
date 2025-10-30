c6e8f07f8fa043ee2b5698a500704293ca23aec3
nikhilkhandelwaal <nikhilcoder061@gmail.com>
2025-10-30 14:20:17 +0530

    loops

---
+++ day10/array2dQuestions.c
@@ -0,0 +1,10 @@
+//     1. WAP find the Addition & Substraction of two 2D array.
+//     2. WAP find the largest and smallest element in a 2D array.
+//     3. WAP find the transpose of a matrix.
+//     4. WAP check if a matrix is Equal or not.
+//     5. WAP print the sum of each row and each column.
+//     6. WAP find the sum of diagonal elements of a square matrix.
+//     7. Diagonal Value using single loop
+//     8. WAP find the upper and lower triangular matrices.
+//     9. WAP check if a given matrix is an identity matrix.
+//     10. Multiplication of Two Matrics
